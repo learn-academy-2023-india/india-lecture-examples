da276ff1a4c5f0eee01b87abaff0744ba64de0cc
SunkissedQueen <baxtercm@outlook.com>
2023-11-13 12:40:41 -0800

    lecture notes up to built-in methods for javascript arrays

--- javascript-arrays.js
+++ javascript-arrays.js
@@ -27,7 +27,7 @@
   // length: number of values in the array
   // console.log(dinner.length) //output: 8
 
-  // visual of index  --->   0  1, 2, 3, 4, 5, 6, 7
+  // visual of index  --->   0  1  2  3  4  5  6  7
   // visual of length --->   1  2  3  4  5  6  7  8
 
   // bracket notation to access different values in the array
@@ -36,35 +36,20 @@
 
   // abstraction: what does the variable represent
 
-// Built-in methods: shortcuts, predetermined functionality
-  // mutators: permanently change the array
-  // .push(): add a value to the end of the array
-  // .pop(): removes the last value from the array
-  // .shift(): removes the value at the 0th index of the array
-  // .unshift(): adds value to the 0th index of the array
+// Built-in methods: shortcuts, predetermined functionality, can be mutators or accessors
+  // MUTATORS: permanently change the array
+    // .push(): add a value to the end of the array
+    // .pop(): removes the last value from the array
+    // .shift(): removes the value at the 0th index of the array
+    // .unshift(): adds value to the 0th index of the array
 
   // console.log(dinner.push("sweet tea"))
-  // output: 9 --> console logging the push method will display the change in the length of the array
+    // output: 9 --> console logging the push method will display the change in the length of the array
 
-  // perform the action first then console log the mutated array
-  dinner.push("sweet tea")
-  console.log(dinner)
-  // output: [
-  //   'cornbread dressing',
-  //   'mashed potatoes and gravy',
-  //   'sweet potato pie',
-  //   'bone-in ham',
-  //   'tamales',
-  //   'baked mac and cheese',
-  //   'hawaiian rolls',
-  //   'smoked turkey',
-  //   'sweet tea'
-  // ]
-
-  // accessor: do not modify the array
-    // concat: combines two or more array
-    var beverage = ["lemonade", "water", "Grey Goose", "Arnold Palmer"]
-    console.log(dinner.concat(beverage))
+  // To perform the push method and its changes
+    // perform the action first then console log the mutated array
+    dinner.push("sweet tea")
+    console.log(dinner)
     // output: [
     //   'cornbread dressing',
     //   'mashed potatoes and gravy',
@@ -74,13 +59,60 @@
     //   'baked mac and cheese',
     //   'hawaiian rolls',
     //   'smoked turkey',
-    //   'sweet tea',
-    //   'lemonade',
-    //   'water',
-    //   'Grey Goose',
-    //   'Arnold Palmer'
+    //   'sweet tea'
     // ]
 
+  // ACCESSORS: do not modify the array
+    // concat: combines two or more array
+    var beverage = ["lemonade", "water", "Grey Goose", "Arnold Palmer"]
+    // console.log(dinner.concat(beverage))
+      // output: [
+      //   'cornbread dressing',
+      //   'mashed potatoes and gravy',
+      //   'sweet potato pie',
+      //   'bone-in ham',
+      //   'tamales',
+      //   'baked mac and cheese',
+      //   'hawaiian rolls',
+      //   'smoked turkey',
+      //   'sweet tea',
+      //   'lemonade',
+      //   'water',
+      //   'Grey Goose',
+      //   'Arnold Palmer'
+      // ]
 
-    console.log(dinner)
-    // output: 
+    // console.log(dinner)
+      // output: 
+        // [
+        //   'cornbread dressing',
+        //   'mashed potatoes and gravy',
+        //   'sweet potato pie',
+        //   'bone-in ham',
+        //   'tamales',
+        //   'baked mac and cheese',
+        //   'hawaiian rolls',
+        //   'smoked turkey',
+        //   'sweet tea'
+        // ]
+
+  // To store the changes from the concat method and reference later
+    // store the method in a variable then console log the variable to track the changes
+    var fullMeal = dinner.concat(beverage)
+    console.log(fullMeal)
+    // output:
+      // [
+      //   'cornbread dressing',
+      //   'mashed potatoes and gravy',
+      //   'sweet potato pie',
+      //   'bone-in ham',
+      //   'tamales',
+      //   'baked mac and cheese',
+      //   'hawaiian rolls',
+      //   'smoked turkey',
+      //   'sweet tea',
+      //   'lemonade',
+      //   'water',
+      //   'Grey Goose',
+      //   'Arnold Palmer'
+      // ]
