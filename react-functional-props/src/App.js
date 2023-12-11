9de37169114cfd708b34e2c72e5f75d332dd5647
SunkissedQueen <baxtercm@outlook.com>
2023-12-11 13:45:00 -0800

    updating the state variable

--- react-functional-props/src/App.js
+++ react-functional-props/src/App.js
@@ -9,17 +9,29 @@ const App = () => {
   const [vacation, setVacation] = useState(
     [
       { destination: "Montego Bay", travelers: 0, selected: false },
-      { destination: "Bora Bora", travelers: 0, selected: false }
+      { destination: "Bora Bora", travelers: 0, selected: false },
+      { destination: "Dubai", travelers: 0, selected: false }
     ]
   )
 
   // debugging
-  // console.log("vacation: ", vacation)
+  console.log("vacation: ", vacation)
+
+  // function to update state variables
+  const vacationPlan = (selectedTrip) => {
+    // console.log("selectedTrip data sent from Trip.js: ", selectedTrip)
+    // console.log("vacation after selecting the button: ", vacation)
+    vacation[selectedTrip].selected = true
+    // vacation[selectedTrip].travelers = vacation[selectedTrip].travelers + 1 // long hand
+    vacation[selectedTrip].travelers += 1 // short hand
+    setVacation([...vacation])
+  }
+  
   // display on the UI
   return(
     <>
       <h1>Jingle All the Way as We Go on Vacay!</h1>
-      <Trip vacation={vacation} />
+      <Trip vacation={vacation} vacationPlan={vacationPlan} />
     </>
   )
 }
--- react-functional-props/src/components/Trip.js
+++ react-functional-props/src/components/Trip.js
@@ -3,28 +3,29 @@ import React from "react"
 
 // functional component declaration
 const Trip = (props) => {
-  console.log("vacation: ", props.vacation)
+  // console.log("props: ", props)
+  // console.log("vacation: ", props.vacation)
   // grabbing the destination name at the 0th index
   // console.log("destination: ", props.vacation[0].destination)
   // console.log("traveler: ", props.vacation[0].travelers)
+  // console.log("function: ", props.vacationPlan)
 
   // display on the UI
   return(
     <>
-      <ul>
-        {props.vacation.map((value, index) => {
-          return(
-            <>
-              <li key={index}>
-                {value.destination} - Total travelers: {value.travelers}
-              </li>
-              {value.selected && 
-                <p>Thank you for reserving a spot.</p>
-              }
-            </>
-          )
-        })}
-      </ul>
+      {props.vacation.map((value, index) => {
+        return(
+          <ul key={index}>
+            <li>
+              {value.destination} - Total travelers: {value.travelers}
+            </li>
+            <button onClick={() => props.vacationPlan(index)}> Select this trip </button>
+            {value.selected && 
+              <p>Thank you for reserving a spot.</p>
+            }
+          </ul>
+        )
+      })}
     </>
   )
 }
