bdeebd1760b8d88e60bad4431b2efed1d9656485
Sarah Proctor <sproctor950@gmail.com>
2024-01-02 11:57:57 -0800

    adds ruby intro notes and lecture examples

---
+++ ruby-intro.rb
@@ -0,0 +1,21 @@
+# Ruby Intro 1/2/2024
+
+# file extension is .rb
+# ruby ruby-intro.rb 
+# p - show an output of your code
+
+my_name = 'sarah'
+# p my_name
+# => "sarah"
+# puts my_name
+# => sarah
+# put string - stringify the output
+
+p [2, 3, 4, 5]
+# => [2, 3, 4, 5]
+puts [2, 3, 4, 5]
+# # =>
+# 2
+# 3
+# 4
+# 5
